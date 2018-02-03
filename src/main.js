c1d1bfacfd3296162f8d80814a4d44aeb643354f
Keven Villeneuve <keven.villeneuve@gmail.com>
2018-02-03 14:45:02 -0500

    basic scroller

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ var scene = null;
 var camera = null;
 var renderer = null;
 var controls = null;
+var cube2 = null;
+var sphere = null;
 
 // Events & callbacks
 document.addEventListener("load", onLoad());
@@ -41,20 +43,39 @@ function initRenderer() {
 	camera.lookAt(new THREE.Vector3(0,0,1));
 
 	// controls
-	controls = new THREE.OrbitControls(camera, renderer.domElement);
-	controls.target.set(0,0,1);
+	//controls = new THREE.OrbitControls(camera, renderer.domElement);
+	//controls.target.set(0,0,1);
 
-	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+	var geometry = new THREE.BoxGeometry( 10, 1000, 0.001 );
+	var material = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
 	var cube = new THREE.Mesh( geometry, material );
+	cube.position.set(0,1000/2,0);
 	scene.add( cube );
+
+	var geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
+	var material2 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+	cube2 = new THREE.Mesh( geometry2, material2 );
+	cube2.position.set(0,80,0);
+	scene.add( cube2 );
+
+	var geoS = new THREE.SphereGeometry( 0.5, 32, 32 );
+	var matS = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+	sphere = new THREE.Mesh( geoS, matS );
+	sphere.position.set(0,1,0);
+	scene.add( sphere );
 
 	add3DAxis();
 	onRender(); // start update loop
 }
 
 function onUpdate() {
-	controls.update();
+	//controls.update();
+	camera.position.y += 0.5;
+	sphere.position.y += 0.5;
+
+	var time = performance.now() * 0.005;
+	sphere.position.x = Math.sin( time * 0.7 ) * 1 + 0;
+	//cube2.position.y += 0.05;
 }
 
 function add3DAxis() {
