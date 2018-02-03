b319a8bc12d24301b6af4474f24852dd7717db59
Maxim Mikhaylov <lawnboymadmax@gmail.com>
2018-02-03 18:46:47 -0500

    moved Myo controls to separate file

---
+++ public/src/myo_controls.js
@@ -0,0 +1,23 @@
+Myo.connect();
+
+var playerXPos = null;
+
+function initMyo(){
+	console.log("test");
+
+	Myo.on('fist', function(){
+
+		console.log('Hello Myo!');
+		this.vibrate();
+		this.zeroOrientation();
+		requestAnimationFrame(onRender);
+		onUpdate();
+		renderer.render(scene, camera);
+
+	});
+	Myo.on('imu', function(data){
+		playerXPos = data.orientation.y / 0.35;
+		console.log(playerXPos);
+	});
+	
+}
